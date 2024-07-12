6bf8260cad3563a14e52e8fdd4e2cdc615687b66
gabPinedaP04 <gab.pinedap04@gmail.com>
2024-07-12 17:34:54 -0600

    commit de titulos

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,7 +4,11 @@ import { Layout } from "../components/Layout";
 export const IndexPage: FC = () => {
 	return (
 		<Layout>
-			<div>Hello main (new update!)</div>
+
+
+			<h1 className="text-5xl font-extrabold text-white display:block text-center  bg-blue-500 pt-2 pb-2">Blue Notes</h1>
+
+
 		</Layout>
 	);
 };
